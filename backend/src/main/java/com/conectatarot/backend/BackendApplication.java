88f16fc6ff108f9250af41ffab683bb3ab45999d
oscar clavijo <os.clavijo@duocuc.cl>
2026-06-19 19:45:43 -0400

    cambiar zona horaria de backend

--- backend/src/main/java/com/conectatarot/backend/BackendApplication.java
+++ backend/src/main/java/com/conectatarot/backend/BackendApplication.java
@@ -2,12 +2,20 @@ package com.conectatarot.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Santiago"));
+		System.out.println("Zona horaria del backend configurada en: " + TimeZone.getDefault().getID());
 	}
 
 }
